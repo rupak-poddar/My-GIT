c6b8f0ef33dc4d46bd43ea276900bb97104d9d58
rupak-poddar <45912544+rupak-poddar@users.noreply.github.com>
2019-04-10 16:53:45 +0300

    ABCD

--- src/com/poddar/rupak/git/MyClass.java
+++ src/com/poddar/rupak/git/MyClass.java
@@ -5,7 +5,6 @@ public class MyClass {
 	public static void main(String[] args) {
 
 		System.out.println("Hello, this is Rupak!");
-		System.out.println("Hello, this is Anesh!");
 		System.out.println("Hello, this is Rupak Poddar!");
 
 	}
