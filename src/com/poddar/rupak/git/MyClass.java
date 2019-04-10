efe9c4793e01ace92e4888debcb834b1c484b000
Rupak Poddar <fin.rupak1983@gmail.com>
2019-04-10 16:12:17 +0300

    A

--- src/com/poddar/rupak/git/MyClass.java
+++ src/com/poddar/rupak/git/MyClass.java
@@ -5,8 +5,6 @@ public class MyClass {
 	public static void main(String[] args) {
 
 		System.out.println("Hello, this is Rupak!");
-		System.out.println("I am doing good!");
-		System.out.println("how are you?");
 
 	}
 
