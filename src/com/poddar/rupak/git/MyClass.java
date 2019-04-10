6267049236826828b81eaecc28fa70e2f2c7e41a
rupak-poddar <45912544+rupak-poddar@users.noreply.github.com>
2019-04-10 17:17:14 +0300

    fghfghfh

--- src/com/poddar/rupak/git/MyClass.java
+++ src/com/poddar/rupak/git/MyClass.java
@@ -4,7 +4,6 @@ public class MyClass {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello, this is Rupak!");
 		System.out.println("Hello, this is Limpa Poddar!");
 		System.out.println("Hello, this is Anesh Poddar!");
 		System.out.println("Hello, this is Adhir Poddar!");
